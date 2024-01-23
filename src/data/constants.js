48e36b357a682fe60f8b42ab52bb677ae95a6a36
wako1234 <akpwajahat@gmail.com>
2024-01-24 01:38:45 +0500

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,330 @@
+export const Bio = {
+  name: "Wajahat Ali Khan",
+  roles: ["React Native Developer", "React JS Developer", "Programmer"],
+  description:
+    "I am someone who's always up for new challenges, eager to learn and deliver great results. I have a positive attitude, love to grow, and I'm ready to make a meaningful contribution to achieve success.",
+  github: "https://github.com/wako1234",
+  resume:
+    "https://drive.google.com/file/d/1BE2I8RBa_M87rSwxKitEpZbyDTBiMK1l/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/wajahat-ali-khan-67498185/",
+  // twitter: "https://twitter.com/wajahat40",
+  // insta: "https://www.instagram.com/rishav_chanda/",
+  // facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Native",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "React JS",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png?20221110153201",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://stackdiary.com/wp-content/uploads/2022/10/Tailwind-CSS-15-Component-Libraries-UI-Kits-701x389.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: "https://www.php.net//images/logos/new-php-logo.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Xcode",
+        image: "https://developer.apple.com/news/images/og/xcode-12-og.jpg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image: "https://avatars.githubusercontent.com/u/10251060?s=280&v=4",
+      },
+      {
+        name: "Adobe XD",
+        image: "https://avatars.githubusercontent.com/u/41522403?s=280&v=4",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://raw.githubusercontent.com/github/explore/05d0f0dfceafd861bdf2b53559399dae7b2e2d8b/topics/figma/figma.png",
+      },
+      {
+        name: "NPM",
+        image: "https://static-00.iconduck.com/assets.00/npm-icon-2048x2048-wm0mnkz6.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    // img: "https://drive.google.com/uc?export=view&id=1_e32rscuQMRZr-1YbBO1n4B0p-OkDPfj",
+    role: "React Native / Reactjs Engineer",
+    company: "Penguin Technologies",
+    date: "June 2023 - Present",
+    desc: `End-to-end development of mobile applications, from conceptualization to publication on the Google Play Store and App Store. Building reusable components and translating UI from mockups.Practicing and implementing clean code.Utilize React Native CLI and state management tools such as Redux and React Query to efficiently handle and manage the application's state, ensuring a robust and responsive user interface.
+    `,
+    skills: [
+      "React Native",
+      "ReactJS",
+      "JavaScript",
+      "Next Js",
+      "Redux",
+      "Tailwind",
+      "Realm",
+      "Firebase",
+      "Jest",
+      "MongoDB",
+      "REST API",
+      "Adobe XD",
+      "Figma",
+    ],
+    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    // img: "https://drive.google.com/uc?export=view&id=1pgBz09oy2jNTK5MNXcHvfSPXTQYsuQnN",
+    role: "React Native Engineer",
+    company: "WQSoftwares",
+    date: "April 2023 - June 2023",
+    desc: "Developing new features and revamping existing ones using React Native Expo.Tasks incorporated UI design, utilisation of State Management libraries along community packages for Apps development.",
+    skills: [
+      "React Native Expo",
+      "JavaScript",
+      "Redux",
+      "Payment Gateways",
+      "Jest",
+      "REST API",
+      "Postman",
+      "Adobe XD",
+    ],
+    // doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    // img: "https://drive.google.com/uc?export=view&id=1TNq78p-fwGADxVlBFuGCpzaPbdntjqR7",
+    role: "Software Developer",
+    company: "Puretech",
+    date: "July 2021 - August 2022",
+    desc: "Developing and maintaining e-commerce platform.",
+    skills: ["PHP", "HTML", "CSS", "JavaScript", "Payment Gateways"],
+  },
+  {
+    id: 2,
+    // img: "https://drive.google.com/uc?export=view&id=1A5flESQxLMCKT5KWKvfWWSplSE8HM-oI",
+    role: "Web Developer | Intern",
+    company: "FamzSolutions",
+    date: "January 2017 - June 2017",
+    desc: "Building and maintaining frontend websites using HTML, CSS and Javascript.",
+    // doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://pbs.twimg.com/profile_images/1545162107565506564/hYkOfSKy_400x400.jpg",
+    school: "Institute of Management Sciences, Peshawar, Pakistan",
+    date: "Nov 2013 - March 2018",
+    grade: "3.33 CGPA",
+    desc: "Final Project is Smart Attendance based on OpencV",
+    degree: "Bachelor of Science - BS in Software Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.edarabia.com/wp-content/uploads/2018/07/islamia-college-peshawar-pakistan.png",
+    school: "Islamia College, Peshawar, Pakistan",
+    date: "Sept 2011 - June 2013",
+    grade: "79%",
+    degree: "Pre-Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "HCL Driver",
+    // date: "Jun 2023 - Jul 2023",
+    description:
+      "Introducing Driver application, the innovative app that seamlessly integrates your bike rides with on-the-go delivery services! Become a driver and effortlessly earn money while cruising around town on your bike. With our user-friendly interface and live Google Maps integration, you can efficiently navigate to destinations, pick up orders, and make timely deliveries, all while enjoying the freedom of two-wheel travel.",
+    image:
+      "https://play-lh.googleusercontent.com/b1qyEpm-oRFhoE14-Ipdw1H-DhVhkY61UsU6RcamfCc6TcXxk6txTUOr3m5OtdTWBxDh=w5120-h2880-rw",
+    tags: [
+      "React Native Expo",
+      "JavaScript",
+      "Redux",
+      "Payment Gateways",
+      "REST API",
+      "Jest",
+      "Postman",
+      "Adobe XD",
+    ],
+    // category: "web app",
+    // github: "https://github.com/wako1234/HCL-driver",
+    webapp:
+      "https://play.google.com/store/apps/details?id=com.hcldeliver.driver&hl=en",
+  },
+  {
+    id: 0,
+    title: "HCL Delivery",
+    description:
+      "Introducing HCL Delivery application, the innovative app that seamlessly integrates customer with rider with on-the-go delivery services.",
+    image:
+      "https://play-lh.googleusercontent.com/oTSHNZIjFokx6pk3dtdySXNNOCtQ2MMW5oQXISO5WS6gEgzAC0B77PYgoaW0ERLOOA=w1052-h592-rw",
+    tags: [
+      "React Native Expo",
+      "JavaScript",
+      "Redux",
+      "Payment Gateways",
+      "REST API",
+      "Jest",
+      "Postman",
+      "Adobe XD",
+    ],
+    // github: "https://github.com/wako1234/HCL-Customer",
+    webapp:
+      "https://play.google.com/store/apps/details?id=com.hcldelivery&hl=en",
+  },
+  {
+    id: 1,
+    title: "Programming Books",
+    description: `Embark on an exciting journey with "Programming Books: Learn Code," the ultimate app for those eager to delve into the world of coding. Discover a treasure trove of programming books tailored for beginners taking their first steps in coding and seasoned pros looking to enhance their skills. Whether you're eager to learn programming languages or simply want to learn to code, this app is your gateway to a vast collection of free and paid programming books`,
+    image:
+      "https://play-lh.googleusercontent.com/0HgsHSKv_8vtWoYMHInfkXlz1bVmj68ZZ_yFJj8dC-CVILxdAFQMaZCkluWNt-ebLSAS=w480-h960-rw",
+    tags: [
+      "React Native",
+      "JavaScript",
+      "React Query",
+      "Redux",
+      "Realm",
+      "Firebase",
+      "REST API",
+      "Jest",
+      "Figma",
+    ],
+    // github: "https://github.com/wako1234/Programming-Books",
+    webapp:
+      "https://play.google.com/store/apps/details?id=com.learn.coding.books",
+  },
+  {
+    id: 2,
+    title: "Car Maintenance Tracking",
+    description:
+      "Welcome to Car Maintenance Tracking! Your go-to solution for hassle-free car management. Keep your car details organized, track maintenance, and calculate key metrics effortlessly.",
+    image:
+      "https://play-lh.googleusercontent.com/hdcRZznebdsavn8NbYwfGS4TrZikorZBa9eU3MLQuRIinO09cfMlux9NiReWNitcnsc=w480-h960-rw",
+    tags: [
+      "React Native",
+      "JavaScript",
+      "Redux",
+      "Realm",
+      "Firebase",
+      "REST API",
+      "Jest",
+      "Figma",
+    ],
+    // github: "https://github.com/wako1234/Car-Maintenance-Tracking",
+    webapp:
+      "https://play.google.com/store/apps/details?id=com.carmaintenancetracking.app",
+  },
+  {
+    id: 3,
+    title: "General Science Books",
+    description:
+      "Embark on an enlightening journey with our app, where you can effortlessly explore an extensive collection of general science books. Dive into a world where you can study science and engineering books and unlock a vast array of resources covering Psychology, Mathematics, Astronomy, and more.",
+    image:
+      "https://play-lh.googleusercontent.com/hel5dqfCsdB_btKDxxbD0w1v-ov55OJ9x85F6AS16EvSxOQKnVqmbTLAYUiV-py9Sw=w480-h960-rw",
+    tags: [
+      "React Native",
+      "JavaScript",
+      "Redux",
+      "React Query",
+      "Realm",
+      "Firebase",
+      "REST API",
+      "Jest",
+      "Figma",
+    ],
+    // github: "https://github.com/wako1234/General-Science-Books",
+    webapp:
+      "https://play.google.com/store/apps/details?id=com.study.general.sciencebooks.app",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
